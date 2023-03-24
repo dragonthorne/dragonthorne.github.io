48df7dd398bfa55e1c2aa36f92dc45b108ac0350
Thorne <125685967+dragonthorne@users.noreply.github.com>
2023-03-24 13:50:52 -0500

    Webring thingy test

---
+++ webring.js
@@ -0,0 +1,27 @@
+import webringJSON from "/webring.json"
+let sites = JSON.parse(webringJSON);
+
+let prev;
+let curIndex = 0;
+let next;
+
+for (let i = 0; i < sites.length; i++) {
+    if (sites[i].url == window.location.href) {
+        curIndex = i;
+        break;
+    }
+}
+
+if (sites[0].url == window.location.href) {
+    prev = sites[sites.length-1];
+} else {
+    prev = sites[curIndex-1];
+}
+
+if (sites[sites.length-1].url == window.location.href) {
+    next = sites[0];
+} else {
+    next = sites[curIndex+1];
+}
+
+let html = '<nav style="a {font-size: 12px; padding: initial; color: black; text-decoration: none; margin: 0px;}">|<a'+ prev.url +'>'+ prev.name +'</a>|\n<p>[placeholder] Webring</p>|<a'+ next.url +'>'+ next.name +'</a>|</nav>'
